60e5cb8d41d1182b2e72b02a994d1f4948a31378
Julian Vos <post@julianvos.nl>
2023-09-11 23:28:31 +0200

    feat: footer is now a grid and added github links to bottom right

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,15 +1,41 @@
-import { Copyright } from '@mui/icons-material';
+import { Code, Copyright, GitHub, Source } from '@mui/icons-material';
 import { DateTime } from 'luxon';
-import { Box } from '@mui/material';
+import { Box, IconButton, Tooltip } from '@mui/material';
 import Link from 'next/link';
 import styles from '@/app/styles.module.css';
+import Grid2 from '@mui/material/Unstable_Grid2';
 
 export default function Footer() {
     return (
-        <Box sx={{ textAlign: 'center', mt: 10 }}>
-            <Copyright fontSize="small" sx={{ verticalAlign: 'middle' }} /> Copyright {DateTime.now().year} Julian Vos
-            <br />
-            <Link className={styles.link} href="/privacy-policy">Privacy policy</Link> | <Link className={styles.link} href="/terms-of-service">Terms of service</Link>
-        </Box>
+        <Grid2 container spacing={2} sx={{ mt: 10 }}>
+            <Grid2 xs={4} />
+            <Grid2 xs={4}>
+                <Box sx={{ textAlign: 'center' }}>
+                    <Copyright fontSize="small" sx={{ verticalAlign: 'middle' }} /> Copyright {DateTime.now().year} Julian Vos
+                    <br />
+                    <Link className={styles.link} href="/privacy-policy">
+                        Privacy policy
+                    </Link>{' '}
+                    |{' '}
+                    <Link className={styles.link} href="/terms-of-service">
+                        Terms of service
+                    </Link>
+                </Box>
+            </Grid2>
+            <Grid2 xs={4}>
+                <Box sx={{ textAlign: 'end' }}>
+                    <Tooltip title="My GitHub">
+                        <IconButton href="https://github.com/Ghoelian" target="_BLANK" rel="noopener noreferrer">
+                            <GitHub />
+                        </IconButton>
+                    </Tooltip>
+                    <Tooltip title="This website's source">
+                        <IconButton href="https://github.com/Ghoelian/julianvos.nl-next" target="_BLANK" rel="noopener noreferrer">
+                            <Code />
+                        </IconButton>
+                    </Tooltip>
+                </Box>
+            </Grid2>
+        </Grid2>
     );
 }
