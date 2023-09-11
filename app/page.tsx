3014cb42dc4315a9cc4163d1012a04ae54390959
Julian Vos <post@julianvos.nl>
2023-09-11 23:28:03 +0200

    chore: make section margin bigger

--- app/page.tsx
+++ app/page.tsx
@@ -6,27 +6,28 @@ import Contact from '@/app/components/contact';
 import Footer from '@/app/components/footer';
 
 export default function Home() {
+    const sectionMargin = 8;
     return (
         <>
-            <Box sx={{ m: 4 }}>
+            <Box sx={{ mb: sectionMargin }}>
                 <Typography variant="h2" sx={{ textAlign: 'center' }}>
                     About me
                 </Typography>
                 <AboutMe />
             </Box>
-            <Box sx={{ m: 4 }}>
+            <Box sx={{ my: sectionMargin }}>
                 <Typography variant="h2" sx={{ textAlign: 'center' }}>
                     Portfolio
                 </Typography>
                 <Portfolio />
             </Box>
-            <Box sx={{ m: 4 }}>
+            <Box sx={{ my: sectionMargin }}>
                 <Typography variant="h2" sx={{ textAlign: 'center' }}>
                     Experience
                 </Typography>
                 <Experience />
             </Box>
-            <Box sx={{ m: 4 }}>
+            <Box sx={{ my: sectionMargin }}>
                 <Typography variant="h2" sx={{ textAlign: 'center' }}>
                     Contact
                 </Typography>
