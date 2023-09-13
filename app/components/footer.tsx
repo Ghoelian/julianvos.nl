c6d8407c83877e68de64364d8ef2e44f16fd8709
Julian Vos <post@julianvos.nl>
2023-09-13 22:47:36 +0200

    feat: footer sticks to bottom

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -7,35 +7,49 @@ import Grid2 from '@mui/material/Unstable_Grid2';
 
 export default function Footer() {
     return (
-        <Grid2 container spacing={2} sx={{ mt: 10 }}>
-            <Grid2 xs={4} />
-            <Grid2 xs={4}>
-                <Box sx={{ textAlign: 'center' }}>
-                    <Copyright fontSize="small" sx={{ verticalAlign: 'middle' }} /> Copyright {DateTime.now().year} Julian Vos
-                    <br />
-                    <Link className={styles.link} href="/privacy-policy">
-                        Privacy policy
-                    </Link>{' '}
-                    |{' '}
-                    <Link className={styles.link} href="/terms-of-service">
-                        Terms of service
-                    </Link>
-                </Box>
+        <Box
+            sx={{
+                position: 'absolute',
+                left: 0,
+                bottom: 0,
+                height: {
+                    sm: '200px',
+                    md: '155px'
+                },
+                width: '100%',
+                overflow: 'hidden'
+            }}
+        >
+            <Grid2 container spacing={2} sx={{ mt: 10 }}>
+                <Grid2 xs={4} />
+                <Grid2 xs={4}>
+                    <Box sx={{ textAlign: 'center' }}>
+                        <Copyright fontSize="small" sx={{ verticalAlign: 'middle' }} /> Copyright {DateTime.now().year} Julian Vos
+                        <br />
+                        <Link className={styles.link} href="/privacy-policy">
+                            Privacy policy
+                        </Link>{' '}
+                        |{' '}
+                        <Link className={styles.link} href="/terms-of-service">
+                            Terms of service
+                        </Link>
+                    </Box>
+                </Grid2>
+                <Grid2 xs={3}>
+                    <Box sx={{ textAlign: 'end' }}>
+                        <Tooltip title="My GitHub">
+                            <IconButton href="https://github.com/Ghoelian" target="_BLANK" rel="noopener noreferrer">
+                                <GitHub />
+                            </IconButton>
+                        </Tooltip>
+                        <Tooltip title="This website's source">
+                            <IconButton href="https://github.com/Ghoelian/julianvos.nl-next" target="_BLANK" rel="noopener noreferrer">
+                                <Code />
+                            </IconButton>
+                        </Tooltip>
+                    </Box>
+                </Grid2>
             </Grid2>
-            <Grid2 xs={4}>
-                <Box sx={{ textAlign: 'end' }}>
-                    <Tooltip title="My GitHub">
-                        <IconButton href="https://github.com/Ghoelian" target="_BLANK" rel="noopener noreferrer">
-                            <GitHub />
-                        </IconButton>
-                    </Tooltip>
-                    <Tooltip title="This website's source">
-                        <IconButton href="https://github.com/Ghoelian/julianvos.nl-next" target="_BLANK" rel="noopener noreferrer">
-                            <Code />
-                        </IconButton>
-                    </Tooltip>
-                </Box>
-            </Grid2>
-        </Grid2>
+        </Box>
     );
 }
