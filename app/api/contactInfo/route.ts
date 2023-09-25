929a2a50861ecf67c4a11f4699022be6d6f9197a
Julian Vos <post@julianvos.nl>
2023-09-25 21:38:31 +0200

    feat: require recaptcha before showing contact info

---
+++ app/api/contactInfo/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(request: NextRequest): Promise<NextResponse> {
+    const recaptchaToken = request.nextUrl.searchParams.get('recaptcha-token');
+    const recaptchaSecret = process.env.RECAPTCHA_SECRET;
+
+    if (recaptchaToken === null || recaptchaSecret === undefined) return NextResponse.next({ status: 400 });
+
+    const formData = new FormData();
+
+    formData.append('secret', recaptchaSecret);
+    formData.append('response', recaptchaToken);
+
+    const response = await fetch('https://www.google.com/recaptcha/api/siteverify', {
+        body: formData,
+        method: 'post'
+    });
+
+    const { success } = await response.json();
+
+    if (!success) return NextResponse.next({ status: 403 });
+
+    return NextResponse.json({
+        location: 'Emmen, Drenthe, NL',
+        phone: '+31 6 48744406',
+        email: 'post@julianvos.nl'
+    });
+}
--- app/components/contact.tsx
+++ app/components/contact.tsx
@@ -1,20 +1,61 @@
 import { Call, Email, LocationOn } from '@mui/icons-material';
 import { Box } from '@mui/material';
-import Link from "next/link";
+import Link from 'next/link';
 import styles from '@/app/styles.module.css';
+import { useEffect, useState } from 'react';
+import ReCAPTCHA from 'react-google-recaptcha';
 
 export default function Contact() {
+    const [contactInfo, setContactInfo] = useState<{
+        phone: string;
+        location: string;
+        email: string;
+    }>();
+    const [recaptchaValidated, setRecaptchaValidated] = useState(false);
+
+    const onChange = (val: string | null) => {
+        fetch(`/api/contactInfo?recaptcha-token=${val}`).then(async (result) => {
+            if (result.ok) {
+                setContactInfo(await result.json());
+                setRecaptchaValidated(true);
+            }
+        });
+    };
+
     return (
-        <Box sx={{ textAlign: 'center' }}>
-            <p>
-                <LocationOn sx={{ verticalAlign: 'middle' }} /> Emmen, Drenthe, NL
-            </p>
-            <p>
-                <Call sx={{ verticalAlign: 'middle' }} /> <Link className={styles.link} href="tel:+31-6-48744406">+31 6 48744406</Link>
-            </p>
-            <p>
-                <Email sx={{ verticalAlign: 'middle' }} /> <Link className={styles.link} href="mailto:post@julianvos.nl">post@julianvos.nl</Link>
-            </p>
+        <Box sx={{ display: 'flex', flexDirection: 'column', textAlign: 'center' }}>
+            {recaptchaValidated ? (
+                <>
+                    {contactInfo && (
+                        <>
+                            <p>
+                                <LocationOn sx={{ verticalAlign: 'middle' }} /> {contactInfo.location}
+                            </p>
+                            <p>
+                                <Call sx={{ verticalAlign: 'middle' }} />{' '}
+                                <Link className={styles.link} href="tel:+31-6-48744406">
+                                    {contactInfo.phone}
+                                </Link>
+                            </p>
+                            <p>
+                                <Email sx={{ verticalAlign: 'middle' }} />{' '}
+                                <Link className={styles.link} href="mailto:post@julianvos.nl">
+                                    {contactInfo.email}
+                                </Link>
+                            </p>
+                        </>
+                    )}
+                </>
+            ) : (
+                <ReCAPTCHA
+                    theme="dark"
+                    style={{
+                        alignSelf: 'center'
+                    }}
+                    sitekey="6LeowlIoAAAAAFf6-YZ-Ldi-Tc528e448LnwxgFU"
+                    onChange={onChange}
+                />
+            )}
         </Box>
     );
 }
