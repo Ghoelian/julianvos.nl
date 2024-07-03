dce6f5df75494e76073974ae78f7078509873320
Ghoelian <ghoelian@noreply.codeberg.org>
2024-07-03 09:50:11 +0000

    Update app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,3 @@
-export const runtime = 'edge';
-
 import type { Metadata } from "next";
 import { ReactNode, StrictMode } from "react";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
