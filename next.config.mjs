e0cd80d6bb7d013e521c2ea5c1c995e0b02725ba
Julian Vos <post@julianvos.nl>
2025-03-16 00:46:32 +0100

    chore: add rewrite to matrix well known config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: '/.well-known/matrix',
+                destination: 'https://matrix.julianvos.nl/.well-known/matrix',
+            }
+        ]
+    }
+};
 
 export default nextConfig;
