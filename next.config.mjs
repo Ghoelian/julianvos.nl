3b8915a94aa0420e8ffb68f60ced6aeb7623ccd7
Julian Vos <post@julianvos.nl>
2025-03-16 13:51:32 +0100

    fix: nvm back to redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/.well-known/matrix/:path*',
+                destination: 'https://matrix.julianvos.nl/.well-known/matrix/:path*',
+                permanent: false
+            }
+        ]
+    }
+};
 
 export default nextConfig;
