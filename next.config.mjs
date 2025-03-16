096b62614afc40745126793e84c2f8939bba1da0
Julian Vos <post@julianvos.nl>
2025-03-16 13:01:56 +0100

    chore: use wildcards, and add redirect for _matrix

--- next.config.mjs
+++ next.config.mjs
@@ -3,18 +3,13 @@ const nextConfig = {
     async redirects() {
         return [
             {
-                source: '/.well-known/matrix/server',
-                destination: 'https://matrix.julianvos.nl/.well-known/matrix/server',
+                source: '/.well-known/matrix/*',
+                destination: 'https://matrix.julianvos.nl/.well-known/matrix/*',
                 statusCode: 302
             },
             {
-                source: '/.well-known/matrix/client',
-                destination: 'https://matrix.julianvos.nl/.well-known/matrix/client',
-                statusCode: 302
-            },
-            {
-                source: '/.well-known/matrix/support',
-                destination: 'https://matrix.julianvos.nl/.well-known/matrix/support',
+                source: '/_matrix/*',
+                destination: 'https://matrix.julianvos.nl/_matrix/*',
                 statusCode: 302
             }
         ]
