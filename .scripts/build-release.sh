4d29966e3872f902c07b4714c325c8a54d9bda05
Julian Vos <post@julianvos.nl>
2025-11-02 19:55:37 +0100

    chore: ignore .out and add build script

---
+++ .scripts/build-release.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+BRANCH=$(git branch --show-current)
+
+pushd "$SCRIPT_DIR" || exit 1
+
+pushd ".."
+
+mkdir -p "./.out"
+
+EXCLUDE=$(xargs -a .gitignore -I{} -d'\n' echo --exclude {})
+
+# We want EXCLUDE to be treated as arguments.
+# shellcheck disable=SC2086
+tar --exclude "./.*" $EXCLUDE -czvf "./.out/$BRANCH.tar.gz" ./
+
+popd || exit 1
